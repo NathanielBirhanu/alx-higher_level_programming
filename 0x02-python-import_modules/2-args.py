7a5eead55a4617807c5390e6e6f41e617be84f58
NathanielBirhanu <nbirhanu98@gmail.com>
2023-10-06 12:52:43 -0700

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         print("{} arguments.".format(n))
     elif n == 1:
         print("{} argument:".format(n))
-        print("{} : {}".format(n, sys.argv[n]))
+        print("{}: {}".format(n, sys.argv[n]))
     else:
         print("{} arguments:".format(n))
         for i in range(1, n + 1):
-            print("{} : {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, sys.argv[i]))
