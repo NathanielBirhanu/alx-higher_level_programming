8f44bb6dc5d32b0d375daaead4a3be81e2a2fbf0
NathanielBirhanu <nbirhanu98@gmail.com>
2023-10-09 04:55:24 -0700

    updated

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        Max = 1
+        for y in my_list:
+            if y > Max:
+                Max = y
+        return Max
