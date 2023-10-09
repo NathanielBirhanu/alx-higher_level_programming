db54873839b2ee9581c857713276320cbfb10927
NathanielBirhanu <nbirhanu98@gmail.com>
2023-10-09 03:33:49 -0700

    updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) != 3 or any(len(row) != 3 for row in matrix):
-        print("Invalid matrix dimensions")
-        return
-    for i in range(3):
-        for j in range(3):
-            print("{:d}".format(matrix[i][j]), end=" ")
+    for Row in matrix:
+        for Column in row:
+            print("{:d}".format(Column), end=" " if col != row[-1] else "")
         print("\n")
+
