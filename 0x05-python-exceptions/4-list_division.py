b3d5cdc8c08e9770fa0e742fd8bd54e2802727f5
NathanielBirhanu <nbirhanu98@gmail.com>
2023-10-23 03:59:51 -0700

    updated

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,35 +2,30 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            if i >= len(my_list_1) or i >= len(my_list_2):
-                raise IndexError("out of range")
-            
-            val_1 = my_list_1[i]
-            val_2 = my_list_2[i]
-            
-            if not isinstance(val_1, (int, float)) or not isinstance(val_2, (int, float)):
-                raise TypeError("wrong type")
-            
-            if val_2 == 0:
-                raise ZeroDivisionError("division by 0")
-            
-            result.append(val_1 / val_2)
-        
-        except IndexError as e:
-            print("Error:", e)
-            return
-        
-        except TypeError:
-            print("Error: wrong type")
+            dividend = my_list_1[i]
+        except IndexError:
+            print("out of range")
             result.append(0)
-        
+            continue
+
+        try:
+            divisor = my_list_2[i]
+        except IndexError:
+            print("out of range")
+            result.append(0)
+            continue
+
+        try:
+            division_result = dividend / divisor
         except ZeroDivisionError:
-            print("Error: division by 0")
+            print("division by 0")
             result.append(0)
-        
-        finally:
-            if i >= len(my_list_1) or i >= len(my_list_2):
-                print("Error: out of range")
-                return
-    
+            continue
+        except (TypeError, ValueError):
+            print("wrong type")
+            result.append(0)
+            continue
+
+        result.append(division_result)
+
     return result
