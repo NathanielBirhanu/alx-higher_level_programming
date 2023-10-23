3469fb1917a2e59a8a843e75d13fae059d66dd7f
NathanielBirhanu <nbirhanu98@gmail.com>
2023-10-23 03:54:40 -0700

    updated

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,23 +1,36 @@
 def list_division(my_list_1, my_list_2, list_length):
-    result_list = []
-    try:
-        for i in range(list_length):
-            try:
-                value1 = my_list_1[i]
-                value2 = my_list_2[i]
-                try:
-                    result = float(value1) / float(value2)
-                    result_list.append(result)
-                except ZeroDivisionError:
-                    result_list.append(0)
-                    print("division by 0")
-                except (ValueError, TypeError):
-                    result_list.append(0)
-                    print("wrong type")
-            except IndexError:
-                print("out of range")
-                break
-    except TypeError:
-        pass
-    finally:
-        return result_list
+    result = []
+    for i in range(list_length):
+        try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError("out of range")
+            
+            val_1 = my_list_1[i]
+            val_2 = my_list_2[i]
+            
+            if not isinstance(val_1, (int, float)) or not isinstance(val_2, (int, float)):
+                raise TypeError("wrong type")
+            
+            if val_2 == 0:
+                raise ZeroDivisionError("division by 0")
+            
+            result.append(val_1 / val_2)
+        
+        except IndexError as e:
+            print("Error:", e)
+            return
+        
+        except TypeError:
+            print("Error: wrong type")
+            result.append(0)
+        
+        except ZeroDivisionError:
+            print("Error: division by 0")
+            result.append(0)
+        
+        finally:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                print("Error: out of range")
+                return
+    
+    return result
