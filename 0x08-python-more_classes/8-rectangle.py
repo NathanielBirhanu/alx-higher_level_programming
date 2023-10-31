3d8375ae710d1e8f6a9b683e3923448b99aa459f
NathanielBirhanu <nbirhanu98@gmail.com>
2023-10-31 03:43:10 -0700

    updated

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+
+"""
+This module defines a Rectangle class that represents a rectangle shape.
+
+The Rectangle class provides methods to calculate the area and perimeter of a rectangle,
+as well as comparison of rectangles based on their areas.
+
+Attributes:
+    number_of_instances (int): Keeps track of the number of Rectangle instances created.
+    print_symbol (Any): Symbol used for string representation of the rectangle.
+
+Classes:
+    Rectangle: Represents a rectangle shape and provides methods to operate on rectangles.
+"""
+
+class Rectangle:
+    """
+    Rectangle class defines a rectangle by its width and height.
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle. Defaults to 0.
+            height (int): The height of the rectangle. Defaults to 0.
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    # Rest of the class implementation...
+
+    def __del__(self):
+        """
+        Performs cleanup when the instance is deleted.
+        Prints a farewell message.
+        """
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
