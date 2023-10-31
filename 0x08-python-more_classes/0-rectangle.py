f0444a4209ee971e932b1f59810736c2f91b4b9e
NathanielBirhanu <nbirhanu98@gmail.com>
2023-10-30 22:45:40 -0700

    updated

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+
+"""Module defining a Rectangle class."""
+
 class Rectangle:
+    """
+    A class representing a rectangle shape.
+
+    This class provides a basic representation of a rectangle and does not have any
+    additional methods or attributes at the moment.
+    """
     pass
