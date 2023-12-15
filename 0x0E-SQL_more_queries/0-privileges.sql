608c44aa969208befc20ee5be57267969abd3e90
NathanielBirhanu <nbirhanu98@gmail.com>
2023-12-15 11:40:44 -0800

    more sql task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
-echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "GRANT ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "GRANT ALL PRIVILEGES ON *.* to 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+#!/bin/bash
+
+mysql -hlocalhost -uroot -p -e "SHOW GRANTS FOR 'user_0d_1'@'localhost';"
+mysql -hlocalhost -uroot -p -e "SHOW GRANTS FOR 'user_0d_2'@'localhost';"
