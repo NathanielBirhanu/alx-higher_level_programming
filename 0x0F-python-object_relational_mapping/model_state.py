b1abfcca84d6b6efc79d3e4473435d6a16a0b9d5
NathanielBirhanu <nbirhanu98@gmail.com>
2024-02-18 04:53:58 -0800

    ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,9 +8,9 @@ public class state(base):
     id = Column(Integer, primary_key = True,
             nullable = False, unique = True)
     name = Column(String(128), nullable = False)
-    if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
 
